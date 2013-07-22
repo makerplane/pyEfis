8af75c99dc4ffc38e89f5fb19ac128c6ae3ba8a3
Phil Birkelbach <phil@petrasoft.net>
2013-07-22 11:58:44 -0500

    Add Turn Coordinator

--- pyEfis.py
+++ pyEfis.py
@@ -30,6 +30,7 @@ import hsi
 import airspeed
 import altimeter
 import vsi
+import tc
 
 # This is a container object to hold the callback for the FIX thread
 # which when called emits the signals for each parameter
@@ -84,6 +85,12 @@ def main(test):
     vs = vsi.VSI(w)
     vs.resize(instWidth, instWidth)
     vs.move((w.width()-200)/3*2, instWidth)
+
+    turn = tc.TurnCoordinator(w)
+    turn.resize(instWidth, instWidth)
+    turn.move(0, instWidth)
+    turn.latAcc = -0.1
+
 
     vb = gauges.VerticalBar(w)
     vb.resize(10,150)
@@ -169,6 +176,7 @@ def main(test):
         alt_gauge = QSpinBox(w)
         alt_gauge.setMinimum(0)
         alt_gauge.setMaximum(10000)
+        alt_gauge.setSingleStep(10)
         alt_gauge.setValue(0)
         alt_gauge.move(720,10)
         alt_gauge.valueChanged.connect(alt.setAltimeter)
@@ -186,12 +194,21 @@ def main(test):
         svsi.setValue(0)
         svsi.resize(20,200)
         svsi.move(740,instWidth)
+        
+        stc = QSlider(Qt.Horizontal, w)
+        stc.setMinimum(-6) # deg / sec
+        stc.setMaximum(6)
+        stc.setValue(0)
+        stc.resize(200,20)
+        stc.move(80,instWidth + 30)
+
 
         pitch.valueChanged.connect(a.setPitchAngle)
         roll.valueChanged.connect(a.setRollAngle)
         smap.valueChanged.connect(map.setValue)
         srpm.valueChanged.connect(rpm.setValue)
         svsi.valueChanged.connect(vs.setROC)
+        stc.valueChanged.connect(turn.setRate)
 
     if(config.screenFullSize):
         w.showFullScreen()
---
+++ tc/__init__.py
@@ -0,0 +1,143 @@
+#  Copyright (c) 2013 Neil Domalik, Phil Birkelbach
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ 
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import PyQt4.Qt
+import math
+
+class TurnCoordinator(QWidget):
+    def __init__(self, parent=None):
+        super(TurnCoordinator, self).__init__(parent)
+        self.setStyleSheet("border: 0px")
+        self.setFocusPolicy(Qt.NoFocus)
+        self._rate = 0.0
+        self._latAcc = 0.0
+
+    def resizeEvent(self, event):
+        self.tick_thickness = self.height()/32
+        self.tick_length = self.width()/12
+        self.background = QPixmap(self.width(), self.height())
+        
+        p = QPainter(self.background)
+        p.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(QColor(Qt.white))
+        pen.setWidth(2)
+        brush = QBrush(QColor(Qt.white))
+        p.setPen(pen)
+        self.center = QPointF(p.device().width()/2, p.device().height()/2)
+        self.r = min(self.width(), self.height())/2 - 25
+        p.drawEllipse(self.center, self.r, self.r)
+        
+        # this draws the tick boxes
+        thickness = self.tick_thickness
+        length = self.tick_length
+        rect = QRect(-(self.r), 0-thickness/2, 
+                     length, thickness)
+        p.setBrush(brush)
+        p.save()
+        p.translate(self.center)
+        p.drawRect(rect)
+        p.rotate(-30)
+        p.drawRect(rect)
+        p.rotate(210)
+        p.drawRect(rect)
+        p.rotate(30)
+        p.drawRect(rect)
+        p.restore()
+        
+        # TC Box
+        self.boxHalfWidth = (self.r - length)*math.cos(math.radians(30))
+        self.boxTop = self.center.y() + (self.r - length)*math.sin(math.radians(30)) + thickness
+        rect = QRect(QPoint(self.center.x() - self.boxHalfWidth, self.boxTop),
+                     QPoint(self.center.x() + self.boxHalfWidth, self.boxTop+length))
+        p.drawRect(rect)
+        #Draw the little airplane center
+        p.drawEllipse(self.center, thickness, thickness)
+        # vertical black lines on TC
+        pen.setColor(QColor(Qt.black))
+        pen.setWidth(4)
+        p.setPen(pen)
+        p.drawLine(self.center.x()-length+8, self.boxTop,
+                   self.center.x()-length+8, self.boxTop+length+2)
+        p.drawLine(self.center.x()+length-8, self.boxTop,
+                   self.center.x()+length-8, self.boxTop+length+2)
+        
+
+    def paintEvent(self, event):
+        w = self.width()
+        h = self.height()
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+
+        # Just to make it a bit easier
+        thickness = self.tick_thickness
+        length = self.tick_length
+
+        # Insert Background
+        p.drawPixmap(0,0,self.background)
+
+        # Draw TC Ball
+        pen = QPen(QColor(Qt.black))
+        brush = QBrush(QColor(Qt.black))
+        pen.setWidth(2)
+        p.setPen(pen)
+        p.setBrush(brush)
+        center = QPointF(self.center.x() + self.boxHalfWidth*4*self._latAcc, 
+                         self.boxTop + length/2)
+        p.drawEllipse(center,length/2, length/2)
+        
+        # the little airplane
+        pen.setColor(QColor(Qt.white))
+        brush.setColor(QColor(Qt.white))
+        p.setPen(pen)
+        p.setBrush(brush)
+        
+        x = self.r - length - thickness / 2
+        poly = QPolygon([QPoint(0, -thickness/3),
+                         QPoint(-x, -thickness/8),
+                         QPoint(-x, thickness/8),
+                         QPoint(0, thickness/3),
+                         QPoint(x, thickness/8),
+                         QPoint(x, -thickness/8)])
+        p.translate(self.center)
+        p.rotate(self._rate*10)
+        p.drawPolygon(poly)
+        pen.setWidth(2)
+        p.setPen(pen)
+        p.drawLine(-length/2, -length/2, length/2, -length/2)
+        p.drawLine(0,0,0,-length)
+ 
+    def getRate(self):
+        return self._rate 
+
+    def setRate(self, rate):
+        if rate != self._rate:
+            self._rate = rate
+            self.update()
+
+    turnRate = property(getRate, setRate)
+    
+    def getLatAcc(self):
+        return self._latAcc
+    
+    def setLatAcc(self, acc):
+        if acc != self._latAcc:
+            self._latAcc = acc
+            self.update()
+    
+    latAcc = property(getLatAcc, setLatAcc)
