ed9dae9afcbdcefbff1fc3f2d2120240731ef3d3
neil-d95 <neil@thedomaliks.net>
2014-02-27 23:14:52 -0500

    Code clean up.

--- fgfs/__init__.py
+++ fgfs/__init__.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-import socket, string, threading, Queue
+import socket, threading, Queue
 
 
 class UDP_Process(threading.Thread):
@@ -7,30 +7,28 @@ class UDP_Process(threading.Thread):
     def __init__(self, conn):
         threading.Thread.__init__(self)
         self.queue = conn
-        UDP_IP=""#192.169.215.6"
-        UDP_PORT=34200
+        UDP_IP = ""
+        UDP_PORT = 34200
 
-        self.sock = socket.socket( socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM ) # UDP
+        self.sock = socket.socket(socket.AF_INET,  # Internet
+                             socket.SOCK_DGRAM)  # UDP
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.sock.bind((UDP_IP,UDP_PORT))
+        self.sock.bind((UDP_IP, UDP_PORT))
         self.running = 1
 
     def run(self):
         while self.running:
             #Reads the UDP packet splits then sends it to the Queue
-            data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
+            data, addr = self.sock.recvfrom(1024)  # buffer size is 1024 bytes
             data_test = data.decode()
-            data_test1 = data_test.split(',')
             if (data_test):
                 self.queue.put(data_test)
-            else: pass
+            else:
+                pass
 
     def stop(self):
         self.running = 0
-
-
 
 if __name__ == '__main__':
     queue = Queue.Queue()
@@ -48,19 +46,16 @@ if __name__ == '__main__':
 
     for node in tree.findall('.//node'):
         Name_Value.append(node.text)
-    
-    
 
     while True:
         try:
             data_test = queue.get(0)
-
             for data in data_test:
-               if len(Name_List) == len(Name_Value):
-                   for q, a, d in zip(Name_List, Name_Value, data_test):
-                       print(q, a, d)
-               else:
-                   print('Name value mismatch in :', f)
+                if len(Name_List) == len(Name_Value):
+                    for q, a, d in zip(Name_List, Name_Value, data_test):
+                        print(q, a, d)
+                else:
+                    print('Name value mismatch in :', f)
         except Queue.Empty:
             pass
     t.join()
