2563a67b53a8bd789b19b3a8536712082fac083a
Neil Domalik <neil@thedomaliks.net>
2016-01-05 10:31:17 -0500

    Updated Base to run with Python3.

--- fix/__init__.py
+++ fix/__init__.py
@@ -12,7 +12,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # The idea behind this file is to create an interface to generic aircraft
 # data, regardless of how that data is retrieved.  Right now it is simply
@@ -22,15 +22,17 @@
 import threading
 
 import canbus
-import canfix
+from . import canfix
+
 
 class Parameter(object):
-    def __init__(self, name = None, value = None):
+    def __init__(self, name=None, value=None):
         self.name = name
         self.value = value
         self.failed = False
         self.quaility = False
         self.annunciate = False
+
 
 class Fix(threading.Thread):
     def __init__(self, adapter=None, device=None, bitrate=125):
@@ -45,11 +47,11 @@ class Fix(threading.Thread):
         self._parameterCallback = None
 
     def setParameterCallback(self, function):
-        if callable(function):
+        if isinstance(function, collections.Callable):
             self._parameterCallback = function
         else:
             raise ValueError("Argument is supposed to be callable")
-    
+
     def run(self):
         self.can.connect()
         while(True):
@@ -59,25 +61,25 @@ class Fix(threading.Thread):
                 # if the frame represents a CAN-FIX parameter then we make
                 # a generic FIX parameter and send that to the callback
                 cfobj = canfix.parseFrame(frame)
-                print "Fix Thread parseFrame() returned", cfobj
+                print("Fix Thread parseFrame() returned", cfobj)
                 if isinstance(cfobj, canfix.Parameter):
                     p = Parameter(cfobj.name, cfobj.value)
                     if self._parameterCallback:
                         self._parameterCallback(cfobj)
                     else:
-                        print frame
+                        print(frame)
             except canbus.exceptions.DeviceTimeout:
                 pass
             except canbus.exceptions.BusError:
                 # TODO: Should handle some of these
-                # Maybe after ten or se we 
+                # Maybe after ten or se we
                 pass
             finally:
                 if(self.getout):
                     break
-        print "End of the FIX Thread"
+        print("End of the FIX Thread")
         self.can.disconnect()
-    
+
     def quit(self):
         self.getout = True
         self.join()
