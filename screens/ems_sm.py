067b05a822e980aa65c49dc46261e91956fb96e2
Phil Birkelbach <phil@petrasoft.net>
2019-01-29 16:28:06 -0600

    Progress on small EMS screen

--- screens/ems_sm.py
+++ screens/ems_sm.py
@@ -60,32 +60,47 @@ class Screen(QWidget):
         self.ot.decimalPlaces = 1
         self.ot.dbkey = "OILT1"
 
-        self.fuel = gauges.HorizontalBar(self)
-        self.fuel.name = "Fuel Qty"
-        self.fuel.decimalPlaces = 1
-        self.fuel.dbkey = "FUELQ"
+        self.fuell = gauges.VerticalBar(self)
+        self.fuell.name = "Left"
+        self.fuell.decimalPlaces = 1
+        self.fuell.dbkey = "FUELQ1"
+        self.fuell.showUnits = False
 
-        self.ff = gauges.HorizontalBar(self)
-        self.ff.name = "Fuel Flow"
+        self.fuelr = gauges.VerticalBar(self)
+        self.fuelr.name = "Right"
+        self.fuelr.decimalPlaces = 1
+        self.fuelr.dbkey = "FUELQ2"
+        self.fuelr.showUnits = False
+
+        self.ff = gauges.VerticalBar(self)
+        self.ff.name = "Flow"
         self.ff.decimalPlaces = 1
         self.ff.dbkey = "FUELF1"
+        self.ff.showUnits = False
 
-        self.cht = gauges.HorizontalBar(self)
-        self.cht.name = "Max CHT"
-        # Use a lambda to convert the values internally
-        self.cht.conversionFunction = lambda x: x * (9.0/5.0) + 32.0
-        # This causes the units sent from the server to be overridden
-        self.cht.unitsOverride = u'\N{DEGREE SIGN}F'
-        self.cht.dbkey = "CHTMAX"
+        self.fuelp = gauges.VerticalBar(self)
+        self.fuelp.name = "Press"
+        self.fuelp.decimalPlaces = 0
+        self.fuelp.dbkey = "FUELP1"
+        self.fuelp.showUnits = False
 
-        self.egt = gauges.HorizontalBar(self)
-        self.egt.name = "Avg EGT"
-        # Use a lambda to convert the values internally
-        self.egt.conversionFunction = lambda x: x * (9.0/5.0) + 32.0
-        # This causes the units sent from the server to be overridden
-        self.egt.unitsOverride = u'\N{DEGREE SIGN}F'
-        self.egt.decimalPlaces = 0
-        self.egt.dbkey = "EGTAVG"
+
+        # self.cht = gauges.HorizontalBar(self)
+        # self.cht.name = "Max CHT"
+        # # Use a lambda to convert the values internally
+        # self.cht.conversionFunction = lambda x: x * (9.0/5.0) + 32.0
+        # # This causes the units sent from the server to be overridden
+        # self.cht.unitsOverride = u'\N{DEGREE SIGN}F'
+        # self.cht.dbkey = "CHTMAX"
+        #
+        # self.egt = gauges.HorizontalBar(self)
+        # self.egt.name = "Avg EGT"
+        # # Use a lambda to convert the values internally
+        # self.egt.conversionFunction = lambda x: x * (9.0/5.0) + 32.0
+        # # This causes the units sent from the server to be overridden
+        # self.egt.unitsOverride = u'\N{DEGREE SIGN}F'
+        # self.egt.decimalPlaces = 0
+        # self.egt.dbkey = "EGTAVG"
 
 
 
@@ -105,14 +120,24 @@ class Screen(QWidget):
         self.ot.resize(200, 75)
         self.ot.move(self.width() - 202, 75)
 
-        self.fuel.resize(150, 50)
-        self.fuel.move(self.width() - 150, 280)
 
-        self.ff.resize(150, 50)
-        self.ff.move(self.width() - 150, 330)
 
-        self.cht.resize(150, 50)
-        self.cht.move(self.width() - 150, 380)
 
-        self.egt.resize(150, 50)
-        self.egt.move(self.width() - 150, 430)
+        self.fuell.resize(50,150)
+        self.fuell.move(self.width() - 200, 180)
+
+        self.fuelr.resize(50, 150)
+        self.fuelr.move(self.width() - 150, 180)
+
+        self.ff.resize(50, 150)
+        self.ff.move(self.width() - 100, 180)
+
+        self.fuelp.resize(50, 150)
+        self.fuelp.move(self.width() - 50, 180)
+
+
+        # self.cht.resize(150, 50)
+        # self.cht.move(self.width() - 150, 380)
+        #
+        # self.egt.resize(150, 50)
+        # self.egt.move(self.width() - 150, 430)
