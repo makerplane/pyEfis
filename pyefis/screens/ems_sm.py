df52dbb08c5c3eb94e33955d5030989a06465883
Phil Birkelbach <phil@petrasoft.net>
2019-08-13 09:27:07 -0500

    Make the EMS screen file more configurable.

--- pyefis/screens/ems_sm.py
+++ pyefis/screens/ems_sm.py
@@ -26,6 +26,177 @@ from pyefis.instruments import gauges
 from pyefis.instruments import misc
 import pyavtools.fix as fix
 
+funcTempF = lambda x: x * (9.0/5.0) + 32.0
+funcTempC = lambda x: x
+
+ENGINE_NUMBER = "1"
+#ENGINE_NUMBER = "2"
+
+CYLINDER_COUNT = 4
+#CYLINDER_COUNT = 6
+
+
+def gauge_list(width, height):
+    il = [
+        {
+            "name":"RPM",
+            "type":gauges.ArcGauge,
+            "key":"TACH" + ENGINE_NUMBER,
+            "decPlaces":0,
+            "width":240,
+            "height":120,
+            "x":0,
+            "y":0,
+        },
+        {
+            "name":"MAP",
+            "type":gauges.ArcGauge,
+            "key":"MAP" + ENGINE_NUMBER,
+            "decPlaces":1,
+            "width":240,
+            "height":120,
+            "x":240,
+            "y":0,
+        },
+        {
+            "name":"Oil Press",
+            "type":gauges.HorizontalBar,
+            "key":"OILP" + ENGINE_NUMBER,
+            "decPlaces":1,
+            "width":200,
+            "height":75,
+            "x":width - 202,
+            "y":0,
+        },
+        {
+            "name":"Oil Temp",
+            "type":gauges.HorizontalBar,
+            "key":"OILT" + ENGINE_NUMBER,
+            "decPlaces":1,
+            "units1":u'\N{DEGREE SIGN}F',
+            "unitFunction1":funcTempF,
+            "units2":u'\N{DEGREE SIGN}C',
+            "unitFunction2":funcTempC,
+            "width":200,
+            "height":75,
+            "x":width - 202,
+            "y":75,
+        },
+        {
+            "name":"Fuel",
+            "type":misc.StaticText,
+            "width":200,
+            "height":30,
+            "x":width - 200,
+            "y":170,
+        },
+        {
+            "name":"Left",
+            "type":gauges.VerticalBar,
+            "key":"FUELQ1",
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":width - 200,
+            "y":200,
+        },
+        {
+            "name":"Right",
+            "type":gauges.VerticalBar,
+            "key":"FUELQ2",
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":width - 150,
+            "y":200,
+        },
+        {
+            "name":"Flow",
+            "type":gauges.VerticalBar,
+            "key":"FUELF" + ENGINE_NUMBER,
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":width - 100,
+            "y":200,
+        },
+        {
+            "name":"Press",
+            "type":gauges.VerticalBar,
+            "key":"FUELP" + ENGINE_NUMBER,
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":width - 50,
+            "y":200,
+        },
+        {
+            "name":"Total",
+            "type":gauges.NumericDisplay,
+            "key":"FUELQT",
+            "decPlaces":1,
+            "showUnits":True,
+            "showName":False,
+            "width":90,
+            "height":30,
+            "x":width - 195,
+            "y":355,
+        },
+        {
+            "name":"Volt",
+            "type":gauges.VerticalBar,
+            "key":"VOLT",
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":0,
+            "y":200,
+        },
+        {
+            "name":"Amp",
+            "type":gauges.VerticalBar,
+            "key":"CURRNT",
+            "decPlaces":1,
+            "showUnits":False,
+            "width":50,
+            "height":150,
+            "x":50,
+            "y":200,
+        },
+        {
+            "name":"EGTs",
+            "type":gauges.EGTGroup,
+            "engine": ENGINE_NUMBER,
+            "cylinderCount": CYLINDER_COUNT,
+            "decPlaces":0,
+            "showUnits":False,
+            "width":200,
+            "height":150,
+            "x":150,
+            "y":200,
+        },
+        {
+            "name":"EGT",
+            "type":misc.StaticText,
+            "width":200,
+            "height":30,
+            "x":150,
+            "y":170,
+        },
+
+    ]
+    return il
+
+        # self.egt.resize(200, 30)
+        # self.egt.move(150, 170)
+        # self.egtgroup.resize(200, 150)
+        # self.egtgroup.move(150, 200)
+
 class Screen(QWidget):
     def __init__(self, parent=None):
         super(Screen, self).__init__(parent)
@@ -39,79 +210,36 @@ class Screen(QWidget):
             self.setPalette(p)
             self.setAutoFillBackground(True)
 
-        self.rpm = gauges.ArcGauge(self)
-        self.rpm.name = "RPM"
-        self.rpm.decimalPlaces = 0
-        self.rpm.dbkey = "TACH1"
+        # First we setup a list of the widgets on this screen.  We intantiate
+        # objects for the screen based on the dictionary returned from
+        # gauge_list().  We don't send the width and height yet because we
+        # don't know it at this point.  We'll do this again on a resize to get
+        # the positions and sizes
+        self.widget_list = []
+        ilist = gauge_list(0,0)
+        for item in ilist:
+            if item["type"] == gauges.EGTGroup:
+                keys = []
+                for cyl in range(item["cylinderCount"]):
+                    keys.append("EGT{}{}".format(item["engine"], cyl+1))
+                i = gauges.EGTGroup(self, item["cylinderCount"], keys)
+            elif item["type"] == misc.StaticText:
+                i = misc.StaticText(item["name"], parent = self)
+            else:
+                i = item["type"](self)
+            i.name = item["name"]
+            if "key" in item: i.dbkey = item["key"]
+            if "decPlaces" in item: i.decimalPlaces = item["decPlaces"]
+            if "showUnits" in item: i.showUnits = item["showUnits"]
+            if "showName" in item: i.showName = item["showName"]
+            if "units1" in item: i.unitsOverride1 = item["units1"]
+            if "units2" in item: i.unitsOverride2 = item["units2"]
+            if "unitFunction1" in item: i.conversionFunction1 = item["unitFunction1"]
+            if "unitFunction2" in item: i.conversionFunction2 = item["unitFunction2"]
 
-        self.map_g = gauges.ArcGauge(self)
-        self.map_g.name = "MAP"
-        self.map_g.decimalPlaces = 1
-        self.map_g.dbkey = "MAP1"
+            self.widget_list.append(i)
 
-        self.op = gauges.HorizontalBar(self)
-        self.op.name = "Oil Press"
-        self.op.decimalPlaces = 1
-        self.op.dbkey = "OILP1"
-
-        self.ot = gauges.HorizontalBar(self)
-        self.ot.name = "Oil Temp"
-        # Use a lambda to convert the values internally
-        self.ot.conversionFunction1 = lambda x: x * (9.0/5.0) + 32.0
-        self.ot.conversionFunction2 = lambda x: x
-        # This causes the units sent from the server to be overridden
-        self.ot.unitsOverride1 = u'\N{DEGREE SIGN}F'
-        self.ot.unitsOverride2 = u'\N{DEGREE SIGN}C'
-        self.ot.setUnitSwitching()
-
-        self.ot.decimalPlaces = 1
-        self.ot.dbkey = "OILT1"
-
-        self.fuel = misc.StaticText("Fuel", parent=self)
-
-        self.fuell = gauges.VerticalBar(self)
-        self.fuell.name = "Left"
-        self.fuell.decimalPlaces = 1
-        self.fuell.showUnits = False
-        self.fuell.dbkey = "FUELQ1"
-
-        self.fuelr = gauges.VerticalBar(self)
-        self.fuelr.name = "Right"
-        self.fuelr.decimalPlaces = 1
-        self.fuelr.showUnits = False
-        self.fuelr.dbkey = "FUELQ2"
-
-        self.ff = gauges.VerticalBar(self)
-        self.ff.name = "Flow"
-        self.ff.decimalPlaces = 1
-        self.ff.showUnits = False
-        self.ff.dbkey = "FUELF1"
-
-        self.fuelp = gauges.VerticalBar(self)
-        self.fuelp.name = "Press"
-        self.fuelp.decimalPlaces = 0
-        self.fuelp.showUnits = False
-        self.fuelp.dbkey = "FUELP1"
-
-        self.fuelt = gauges.NumericDisplay(self)
-        self.fuelt.name = "Total"
-        self.fuelt.decimalPlaces = 1
-        self.fuelt.dbkey = "FUELQT"
-        self.fuelt.showUnits = False
-        self.fuelt.showName = False
-
-        self.volts = gauges.VerticalBar(self)
-        self.volts.name = "Volt"
-        self.volts.decimalPlaces = 1
-        self.volts.showUnits = False
-        self.volts.dbkey = "VOLT"
-
-        self.current = gauges.VerticalBar(self)
-        self.current.name = "Amp"
-        self.current.decimalPlaces = 1
-        self.current.showUnits = False
-        self.current.dbkey = "CURRNT"
-
+        # Leaving this alone for now until I can fix the CHT grouping
         self.cht = misc.StaticText("CHT", parent=self)
         self.chts = []
         for x in range(self.cylCount):
@@ -143,38 +271,38 @@ class Screen(QWidget):
         self.chtmax.setUnitSwitching()
         self.chtmax.unitGroup = "Temperature"
         self.chtmax.dbkey = "CHTMAX1"
-
-        self.egt = misc.StaticText("EGT", parent=self)
-        self.egtgroup = gauges.EGTGroup(self, self.cylCount, ["EGT11", "EGT12", "EGT13", "EGT14"])
-
-        self.hobbslabel = misc.StaticText("Engine Time", parent=self)
-
-        self.hobbs = gauges.NumericDisplay(self)
-        self.hobbs.name = "Hobbs"
-        self.hobbs.decimalPlaces = 1
-        self.hobbs.dbkey = "HOBBS1"
-        self.hobbs.alignment = Qt.AlignRight | Qt.AlignVCenter
-        self.hobbs.showUnits = True
-        self.hobbs.smallFontPercent = 0.6
-
-        self.oatlabel = misc.StaticText("OAT", parent=self)
-        self.oatlabel.alignment = Qt.AlignLeft | Qt.AlignVCenter
-
-        self.oat = gauges.NumericDisplay(self)
-        self.oat.name = "OAT"
-        self.oat.decimalPlaces = 1
-        self.oat.dbkey = "OAT"
-        self.oat.alignment = Qt.AlignLeft | Qt.AlignVCenter
-        self.oat.conversionFunction1 = lambda x: x * (9.0/5.0) + 32.0
-        self.oat.conversionFunction2 = lambda x: x
-        self.oat.unitsOverride1 = u'\N{DEGREE SIGN}F'
-        self.oat.unitsOverride2 = u'\N{DEGREE SIGN}C'
-        self.oat.showUnits = True
-        self.oat.setUnitSwitching()
-        self.oat.smallFontPercent = 0.6
-
-        self.timez = misc.ValueDisplay(self)
-        self.timez.dbkey = "TIMEZ"
+        #
+        # self.egt = misc.StaticText("EGT", parent=self)
+        # self.egtgroup = gauges.EGTGroup(self, self.cylCount, ["EGT11", "EGT12", "EGT13", "EGT14"])
+        #
+        # self.hobbslabel = misc.StaticText("Engine Time", parent=self)
+        #
+        # self.hobbs = gauges.NumericDisplay(self)
+        # self.hobbs.name = "Hobbs"
+        # self.hobbs.decimalPlaces = 1
+        # self.hobbs.dbkey = "HOBBS1"
+        # self.hobbs.alignment = Qt.AlignRight | Qt.AlignVCenter
+        # self.hobbs.showUnits = True
+        # self.hobbs.smallFontPercent = 0.6
+        #
+        # self.oatlabel = misc.StaticText("OAT", parent=self)
+        # self.oatlabel.alignment = Qt.AlignLeft | Qt.AlignVCenter
+        #
+        # self.oat = gauges.NumericDisplay(self)
+        # self.oat.name = "OAT"
+        # self.oat.decimalPlaces = 1
+        # self.oat.dbkey = "OAT"
+        # self.oat.alignment = Qt.AlignLeft | Qt.AlignVCenter
+        # self.oat.conversionFunction1 = lambda x: x * (9.0/5.0) + 32.0
+        # self.oat.conversionFunction2 = lambda x: x
+        # self.oat.unitsOverride1 = u'\N{DEGREE SIGN}F'
+        # self.oat.unitsOverride2 = u'\N{DEGREE SIGN}C'
+        # self.oat.showUnits = True
+        # self.oat.setUnitSwitching()
+        # self.oat.smallFontPercent = 0.6
+        #
+        # self.timez = misc.ValueDisplay(self)
+        # self.timez.dbkey = "TIMEZ"
 
     # Find the hightest CHT and highlight it
     # TODO: This could probably be optimized a little better
@@ -193,44 +321,14 @@ class Screen(QWidget):
 
 
     def resizeEvent(self, event):
-        instWidth = self.width() - 150
-        instHeight = self.height() - 60
+        # instWidth = self.width() - 150
+        # instHeight = self.height() - 60
 
-        self.rpm.resize(240, 120)
-        self.rpm.move(0, 0)
+        inst_list = gauge_list(self.width(), self.height())
 
-        self.map_g.resize(240, 120)
-        self.map_g.move(240,0)
-
-        self.op.resize(200, 75)
-        self.op.move(self.width() - 202, 0)
-
-        self.ot.resize(200, 75)
-        self.ot.move(self.width() - 202, 75)
-
-        self.fuel.resize(200, 30)
-        self.fuel.move(self.width() - 200, 170)
-
-        self.fuell.resize(50,150)
-        self.fuell.move(self.width() - 200, 200)
-
-        self.fuelr.resize(50, 150)
-        self.fuelr.move(self.width() - 150, 200)
-
-        self.ff.resize(50, 150)
-        self.ff.move(self.width() - 100, 200)
-
-        self.fuelp.resize(50, 150)
-        self.fuelp.move(self.width() - 50, 200)
-
-        self.fuelt.resize(90, 30)
-        self.fuelt.move(self.width() - 195, 355)
-
-        self.volts.resize(50, 150)
-        self.volts.move(0, 200)
-
-        self.current.resize(50, 150)
-        self.current.move(50, 200)
+        for x, item in enumerate(inst_list):
+            self.widget_list[x].resize(item["width"], item["height"])
+            self.widget_list[x].move(item["x"], item["y"])
 
         chtstartx = 380
         self.cht.resize(200, 30)
@@ -244,21 +342,17 @@ class Screen(QWidget):
         self.chtmaxlabel.move(chtstartx + 10, 360)
         self.chtmax.resize(75, 30)
         self.chtmax.move(chtstartx + 45, 355)
-
-        self.egt.resize(200, 30)
-        self.egt.move(150, 170)
-        self.egtgroup.resize(200, 150)
-        self.egtgroup.move(150, 200)
-
-        self.hobbslabel.resize(100,15)
-        self.hobbslabel.move(self.width()-115, self.height()-85)
-        self.hobbs.resize(110,20)
-        self.hobbs.move(self.width()-115, self.height()-70)
-
-        self.timez.resize(100, 20)
-        self.timez.move(self.width()-115, self.height()-40)
-
-        self.oatlabel.resize(100,15)
-        self.oatlabel.move(10, self.height()-85)
-        self.oat.resize(80,25)
-        self.oat.move(10, self.height()-70)
+        #
+        #
+        # self.hobbslabel.resize(100,15)
+        # self.hobbslabel.move(self.width()-115, self.height()-85)
+        # self.hobbs.resize(110,20)
+        # self.hobbs.move(self.width()-115, self.height()-70)
+        #
+        # self.timez.resize(100, 20)
+        # self.timez.move(self.width()-115, self.height()-40)
+        #
+        # self.oatlabel.resize(100,15)
+        # self.oatlabel.move(10, self.height()-85)
+        # self.oat.resize(80,25)
+        # self.oat.move(10, self.height()-70)
