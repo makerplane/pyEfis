44d246995f58b0a9c5f8d88bad4a2a01fa8c0216
Eric Blevins <eblevins@kmilearning.com>
2024-02-20 18:58:24 -0500

    The listbox can load lists and sort, logic to handle selections is in place, still WIP

--- pyefis/instruments/listbox/__init__.py
+++ pyefis/instruments/listbox/__init__.py
@@ -12,6 +12,7 @@ logger=logging.getLogger(__name__)
 
 import yaml
 import os
+import operator
 
 class ListBox(QGraphicsView):
     def __init__(self, parent=None, lists=[]):
@@ -24,31 +25,44 @@ class ListBox(QGraphicsView):
         self.active_list = list(self.tlists.keys())[0]
         self.columns = len(self.tlists[self.active_list]['display']['columns'])
         self.rows = len(self.tlists[self.active_list]['list'])
-        print(self.tlists)
-        print(self.active_list)
-        print(self.columns)
-        print(self.rows)
-        print(self.width())
+        self.sort = False
+        #print(self.tlists)
+        #print(self.active_list)
+        #print(self.columns)
+        #print(self.rows)
+        #print(self.width())
         #self.loadList()
         #self.insertItem(0, "Red")
         #self.insertItem(1, "Orange")
         #self.insertItem(2, "Blue")
         #self.itemDoubleClicked.connect(self.onClicked)
         self.table = QTableWidget(self)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.table.verticalHeader().setVisible(False)
 
+        self.table.doubleClicked.connect(self.clicked)
 
     def resizeEvent(self,event):
         self.font = QFont()
-        self.font.setPixelSize(qRound(self.height() * 7/100)) # TODO, make this configurable
+        self.font.setPixelSize(qRound(self.height() * 7/100)) # TODO, make this configurable and some of the stuff below
         self.table.setFont(self.font)
+        # TODO Render the name of the list above the list
+        # Set background color, white text on black backgrund?
         #self.setMinimumSize(QSize(500,500))
         self.table.setMinimumWidth(self.width())
         self.table.setMinimumHeight(int(self.height() * 90/100))
         self.table.move(0,int(self.height() * 10/100))
+        self.loadList()
+
+    def loadList(self):
+        print(f"Loading: {self.active_list}")
+
+        self.table.setRowCount(0);
+
         self.column_names = [ item['name'] for item in self.tlists[self.active_list]['display']['columns'] ]
-        self.sort_options = [ item['name'] for item in self.tlists[self.active_list]['display']['columns'] if item['sort'] ]
+        self.sort_options = [ item['name'] for item in self.tlists[self.active_list]['display']['columns'] if item.get('sort', False) ]
 
         self.table.setColumnCount(self.columns)
         self.table.setRowCount( self.rows + len( self.sort_options ) + len(self.tlists) - 1 ) 
@@ -60,30 +74,69 @@ class ListBox(QGraphicsView):
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.table.horizontalHeader().setMaximumSectionSize(int(self.width() * 35/100)) 
         index = 0
+        self.actions = []
         if len(self.tlists) > 1:
             self.table.setItem(index,0, QTableWidgetItem("Select List:"))
+            self.actions.append({'select_list': True})
             index = 1
-        for o in self.sort_options:
+        for c,o in enumerate(self.sort_options):
             self.table.setItem(index,0, QTableWidgetItem("Sort by:"))
             self.table.setItem(index,1, QTableWidgetItem(o))
+            self.actions.append({'sort': True, 'option': c})
             index += 1
-        #print(self.tlists)
-        #print(self.active_list)
-        #print(self.tlists[self.active_list]['list'])
-        for o in self.tlists[self.active_list]['list']:
+        if self.sort:
+            the_list = sorted(self.tlists[self.active_list]['list'], key=operator.itemgetter(self.sort))
+        else:
+            the_list = self.tlists[self.active_list]['list']
+        for o in the_list:
             for c,n in enumerate(self.column_names):
                 self.table.setItem(index,c, QTableWidgetItem(o[n]))
             index += 1
-        self.table.resizeColumnsToContents()
+            self.actions.append({'select': True, 'option': o})
+
+        #self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
-        #self.table.width = self.width()
-        #self.table.show()
+
+    def loadListSelector(self):
+        self.table.setRowCount(0);
+        self.column_names = ["Select a list"]
+        self.table.setColumnCount(1)
+        self.table.setRowCount(len(self.tlists) + 1)
+        self.table.setHorizontalHeaderLabels( self.column_names )
+        self.table.horizontalHeader().setMaximumSectionSize(0)
+        self.actions = []
+        self.table.setItem(0,0,QTableWidgetItem("Return"))
+        # Preserve sort for return option
+        self.actions.append({"load_list": True, "option": self.active_list, "sort": self.sort})
+        for c,l in enumerate(self.tlists):
+            self.table.setItem(c + 1, 0, QTableWidgetItem(l))
+            self.actions.append({"load_list": True, "option": l})
+        #self.table.resizeColumnsToContents()
+        self.table.resizeRowsToContents()
+
 
 #    def resizeEvent(self,event):
 #        pass
 
-    def loadList(self):
+    def clicked(self, item):
+        if self.actions[item.row()].get('select_list', False):
+            print("User wants to load different list")
+            self.loadListSelector()
+        elif self.actions[item.row()].get('load_list', False):
+            print(f"User wants to load the list {self.actions[item.row()]['option']}")
+            self.active_list = self.actions[item.row()]['option']
+            self.sort = self.actions[item.row()].get('sort',False)
+            self.loadList()
+        elif self.actions[item.row()].get('sort', False):
+            print(f"User wants to sort by {self.sort_options[self.actions[item.row()]['option']]}")
+            self.sort = self.sort_options[self.actions[item.row()]['option']]
+            self.loadList()
+        elif self.actions[item.row()].get('select', False):
+            print(f"User selected the row {self.actions[item.row()]}") 
+
+    def show_lists(self):
         pass
+
 #Config:
 #    options:
 #      encoder: FIXID_ENCODER_INPUT <- Moves selection up or down
@@ -131,6 +184,3 @@ class ListBox(QGraphicsView):
 #  | Knox County | K4I3 | 123.900 Mhz|
 
 
-    def onClicked(self, item):
-        print(self.row(item))
-        #QMessageBox.information(self, "Info", item.text())
