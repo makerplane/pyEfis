20bcbfc53eb512fc09e9cec2003412543ca49b66
Eric Blevins <eblevins@kmilearning.com>
2024-03-19 21:28:56 -0400

    ghosting working for name and value in vertical bar

--- pyefis/instruments/gauges/verticalBar.py
+++ pyefis/instruments/gauges/verticalBar.py
@@ -85,7 +85,7 @@ class VerticalBar(AbstractGauge):
 
 
     def setMode(self, args):
-        print(f"Seting mode for {self._dbkey}")
+        #print(f"Seting mode for {self._dbkey}")
         if args.lower() == "normalize":
                 self.normalizeMode = not self._normalizeMode
         elif args.lower() == "peak":
@@ -127,26 +127,33 @@ class VerticalBar(AbstractGauge):
         self.barHeight = self.barBottom - self.barTop
 
         self.nameTextRect = QRectF(0, 0, self.width(), self.smallFont.pixelSize())
+        #if self.name_font_mask:
+        #    self.smallFont.setPointSizeF(helpers.fit_to_mask(self.width() - self.width() / 6, self.smallFont.pixelSize(), self.name_font_mask, self.font_family))
         self.valueTextRect = QRectF(0, self.barBottom + self.text_gap, self.width() -4, self.bigFont.pixelSize())
         if self.font_mask:
-            self.bigFont.setPointSizeF(helpers.fit_to_mask(self.width() - 4, self.bigFont.pixelSize(), self.font_mask, self.font_family))
+            self.bigFont.setPointSizeF(helpers.fit_to_mask(self.width() - self.width() / 6, self.bigFont.pixelSize(), self.font_mask, self.font_family))
 
         self.unitsTextRect = QRectF(0, self.height() - self.smallFont.pixelSize() - self.text_gap, self.width(), self.smallFont.pixelSize() + self.text_gap)
+        if self.name_font_mask:
+            self.smallFont.setPointSizeF(helpers.fit_to_mask(self.width() - self.width() / 6, self.smallFont.pixelSize(), self.name_font_mask, self.font_family))
+
         self.ballRadius = self.barWidth * 0.40
         self.ballCenter = QPointF(self.barLeft + (self.barWidth / 2), self.barBottom - (self.barWidth/2))
 
     def drawValue(self, p, pen):
+        value_align = Qt.AlignCenter
         p.setFont(self.bigFont)
         if self.font_ghost_mask:
+            value_align = Qt.AlignRight
             alpha = self.valueColor.alpha()
             self.valueColor.setAlpha(self.font_ghost_alpha)
             pen.setColor(self.valueColor)
             p.setPen(pen)
-            p.drawText(self.valueTextRect, self.font_ghost_mask, QTextOption(Qt.AlignRight))
+            p.drawText(self.valueTextRect, self.font_ghost_mask, QTextOption(value_align))
             self.valueColor.setAlpha(alpha)
         pen.setColor(self.valueColor)
         p.setPen(pen)
-        p.drawText(self.valueTextRect, self.valueText, QTextOption(Qt.AlignRight))
+        p.drawText(self.valueTextRect, self.valueText, QTextOption(value_align))
 
     def paintEvent(self, event):
         if self.highlight_key:
@@ -164,6 +171,17 @@ class VerticalBar(AbstractGauge):
         p.setPen(pen)
         opt = QTextOption(Qt.AlignCenter)
         if self.show_name:
+            if self.name_font_ghost_mask:
+                opt = QTextOption(Qt.AlignLeft)
+                alpha = self.textColor.alpha()
+                self.textColor.setAlpha(self.font_ghost_alpha)
+                pen.setColor(self.textColor)
+                p.setPen(pen)
+                p.setFont(self.smallFont)
+                p.drawText(self.nameTextRect, self.name_font_ghost_mask, opt)
+                self.textColor.setAlpha(alpha)
+                self.name = self.name.replace(' ', '!')
+                #print(self.name)
             pen.setColor(self.textColor)
             p.setPen(pen)
             p.setFont(self.smallFont)
@@ -183,6 +201,7 @@ class VerticalBar(AbstractGauge):
                 self.drawValue(p, pen)
         if self.show_units:
             # Units
+            opt = QTextOption(Qt.AlignCenter)
             pen.setColor(self.textColor)
             p.setPen(pen)
             p.setFont(self.smallFont)
