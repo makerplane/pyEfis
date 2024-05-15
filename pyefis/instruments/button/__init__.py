1923bc4f3ba715672a7591694ef03854ecf51878
Eric Blevins <eblevins@kmilearning.com>
2024-05-15 19:32:50 -0400

    Added MENUHIDE timeout feature

--- pyefis/instruments/button/__init__.py
+++ pyefis/instruments/button/__init__.py
@@ -306,7 +306,8 @@ class Button(QWidget):
     # Highlight this instrument to show it is the current selection
     def enc_highlight(self,onoff):
         if onoff:
-            self._style['bg_override'] = QColor('orange') 
+            self._style['bg_override'] = QColor('orange')
+            fix.db.set_value('MENUHIDE', False) 
         else:
             self._style['bg_override'] = None 
         self.setStyle()
