8a2e29923b96f266ad670d59e8cae498059b5f49
Eric Blevins <eblevins@kmilearning.com>
2024-06-02 16:11:37 -0400

    Reverted the changes to weston that were made trying to resolve the white border. I think the issue is a bug in QGraphicsView, I changed it to QWidget and it seems to work perfectly fine with just that one change. But if android starts while the android page is not visible it will start up with a 200x200px window and it stays that way. Need to resolve that

--- pyefis/instruments/weston/__init__.py
+++ pyefis/instruments/weston/__init__.py
@@ -6,29 +6,19 @@ import time
 import re
 import os
 
-class Weston(QGraphicsView):
+class Weston(QWidget):
     def __init__(self, parent=None, socket=None, ini=None, command=None, args=None):
         super(Weston, self).__init__(parent)
-        self.started = False
-        self.socket = socket
-        self.weston_cfg = ini
-        self.command = command
-        self.args = args
-
-        self.weston = QProcess(self)
-        westenv = QProcessEnvironment.systemEnvironment()
-        self.weston.setProcessEnvironment(westenv)
-
-
-    def startWeston(self):
-        self.started = True
-        print(self.weston_cfg)
         layout = QVBoxLayout()
         self.setLayout(layout)
         layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
-        #self.weston.start('weston', [f"-c{self.weston_cfg}",'-i0',f"--width={self.width()}","--height={self.height()}",'-Bx11-backend.so',f"-S{self.socket}"])
-        self.weston.start('weston', [f"-c{self.weston_cfg}",'-i0','-Bx11-backend.so',f"-S{self.socket}"])
+
+        self.weston = QProcess(self)
+        westenv = QProcessEnvironment.systemEnvironment()
+            
+        self.weston.setProcessEnvironment(westenv)
+        self.weston.start('weston', [f"-c{ini}",'-i0','-Bx11-backend.so',f"-S{socket}"])
         loop_count = 0
         loop_limit = 15
         while loop_count < loop_limit:
@@ -46,19 +36,11 @@ class Weston(QGraphicsView):
                     self.layout().addWidget(wid)
                     loop_count = loop_limit + 1
                     break
-        # Without this a 1px white border will show up on the left and top
-        # But you can remove this line if this is the first screen to be displayed.
-        # the border only shows up when it is not set as the main screen
-        # Maybe it is a calculation issue
-        # I did confirm it is not the skip grid_layout code causing it.
-        self.move(-1,-1)
-    def resizeEvent(self, event):
-        if not self.started:
-            self.startWeston()
 
     def closeEvent(self, event):
         self.weston.terminate()
         self.weston.waitForFinished(4000)
 
         super(QGraphicsView, self).closeEvent(event)
+
 
