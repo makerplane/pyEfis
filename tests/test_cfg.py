3574d0529cbd16f835cd9bc3d75eec8a22b7c34f
Eric Blevins <eblevins@kmilearning.com>
2024-06-09 14:27:49 -0400

    Created unit tests for test_cfg.py

--- tests/test_cfg.py
+++ tests/test_cfg.py
@@ -4,32 +4,48 @@ from unittest.mock import MagicMock, patch
 from pyefis.cfg import from_yaml
 
 
+def test_missing_path():
+    with pytest.raises(SyntaxError):
+        d = from_yaml(fname='missing_path.yaml')
 
 def test_sub_parent_includes():
-    d = from_yaml('tests/data/cfg/a.yaml')
-    assert d == {'a': {'b': {'c': {'done': True}}, 'b2': {'d': {'end': True}}}}
+    a = from_yaml('tests/data/cfg/test_sub_parent_includesA.yaml')
+    assert a == {'a': {'b': {'c': {'done': True}}, 'b2': {'d': {'end': True}}}}
 
 def test_array_and_preferences():
-    e = from_yaml('tests/data/cfg/array_of_includes.yaml', preferences={"includes": {"PREFERENCEA": "PREFERENCEA.yaml"}})
-    assert e == {'ARRAYS': {'includea': True, 'PREFERENCEA': True}}
+    b = from_yaml('tests/data/cfg/test_array_and_preferences.yaml', preferences={"includes": {"PREFERENCEA": "test_array_and_preferencesA.yaml","PREFERENCEB": "test_array_and_preferencesB.yaml"}})
+    assert b == {'ARRAYS': {'includeC': True, 'includeA': True, 'includeB': True}}
 
 def test_data_type():
-    with pytest.raises(Exception):
-        d = from_yaml('tests/data/cfg/not_string.yaml')
+    with pytest.raises(SyntaxError):
+        c = from_yaml('tests/data/cfg/test_data_type.yaml')
 
 def test_loop_detection_exception():
-    with pytest.raises(Exception):
-        d = from_yaml('tests/data/cfg/loop1.yaml')
+    with pytest.raises(RecursionError):
+        d = from_yaml('tests/data/cfg/test_loop_detection_exception.yaml')
 
-# We want to test:
-# Various loop exceptions
-#
-# include: filename
-# include:
-# - filename1:
-# - filename2:
-# include: PREFERENCE_NAME
-# various uses of 'item'
-# include from root or from same sub-folder
-# include from another folder
+def test_preference_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        e = from_yaml('tests/data/cfg/test_preference_file_not_found.yaml',preferences={"includes":{"NOT_FOUND":"preference_not_found.yaml"}})
+
+def test_no_preferences():
+    with pytest.raises(FileNotFoundError):
+        e = from_yaml('tests/data/cfg/test_no_preferences.yaml')
+
+def test_list_include_missing_items():
+    with pytest.raises(SyntaxError):
+        e = from_yaml('tests/data/cfg/test_list_include_missing_items.yaml')
+
+def test_list_include_missing_items_via_preferences():
+    with pytest.raises(SyntaxError):
+        e = from_yaml('tests/data/cfg/test_list_include_missing_items_pref.yaml',preferences={"includes":{"PREFERENCED": "test_list_include_missing_items_via_preferencesD.yaml","PREFERENCEE": "preferencee.yaml"}})
+
+def test_list_include_missing_items_via_preferences_nested():
+    with pytest.raises(SyntaxError):
+        e = from_yaml('tests/data/cfg/test_list_include_missing_items_pref_nested.yaml',preferences={"includes":{"PREFERENCEF": "test_list_include_missing_items_via_preferences_nestedF.yaml","PREFERENCEG": "test_list_include_missing_items_via_preferences_nestedG.yaml"}})
+
+def test_list_bpath_preference():
+    e = from_yaml('tests/data/cfg/test_list_bpath_preference.yaml', preferences={"includes":{"A":"sub/sub2/test_list_bpath_preferenceA.yaml","B":"test_list_bpath_preferenceB.yaml","C":"test_list_bpath_preferenceC.yaml"}})
+    assert e == {'test': [{'b_id': 5}, {'c_id_is': 5}, 5, 'Ten']}
+
 
