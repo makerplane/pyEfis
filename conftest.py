617810bd47a86f523ab5d0bb473dbcad7805d851
Eric Blevins <eblevins@kmilearning.com>
2024-06-12 21:35:48 -0400

    Added missing unit_font_mask to numerical_display screenshot

--- conftest.py
+++ conftest.py
@@ -22,60 +22,29 @@ fix.db.get_item("TEST").bad = False
 fix.db.get_item("TEST").fail = False
 
 
-fix.db.define_item("NUMOK", "Number OK", 'float', 0, 100, 'degC', 50000, "")
-fix.db.set_value("NUMOK", 50.19)
-fix.db.get_item("NUMOK").bad = False
-fix.db.get_item("NUMOK").fail = False
+def create_numbers(key,value,old=False,bad=False,fail=False,annunciate=False):
+    fix.db.define_item(key, "Number", 'float', 0, 100, 'degC', 50000, "Min,Max,highWarn,highAlarm,lowWarn,lowAlarm")
+    fix.db.set_value(key, value)
+    fix.db.get_item(key).old = old
+    fix.db.get_item(key).bad = bad
+    fix.db.get_item(key).fail = fail
+    fix.db.get_item(key).annunciate = annunciate
+    fix.db.get_item(key).set_aux_value("Min",0)
+    fix.db.get_item(key).set_aux_value("lowAlarm",10)
+    fix.db.get_item(key).set_aux_value("lowWarn",20)
+    fix.db.get_item(key).set_aux_value("highWarn",80)
+    fix.db.get_item(key).set_aux_value("highAlarm",90)
+    fix.db.get_item(key).set_aux_value("Max",100)
 
-fix.db.define_item("NUMOLD", "Number OK", 'float', 0, 100, 'degC', 5, "")
-fix.db.set_value("NUMOLD", 50.19)
-fix.db.get_item("NUMOLD").old = True
-fix.db.get_item("NUMOLD").bad = False
-fix.db.get_item("NUMOLD").fail = False
-
-fix.db.define_item("NUMBAD", "Number OK", 'float', 0, 100, 'degC', 50000, "")
-fix.db.set_value("NUMBAD", 50.19)
-fix.db.get_item("NUMBAD").old = False
-fix.db.get_item("NUMBAD").bad = True
-fix.db.get_item("NUMBAD").fail = False
-
-fix.db.define_item("NUMFAIL", "Number OK", 'float', 0, 100, 'degC', 50000, "")
-fix.db.set_value("NUMFAIL", 50.19)
-fix.db.get_item("NUMFAIL").old = False
-fix.db.get_item("NUMFAIL").bad = False
-fix.db.get_item("NUMFAIL").fail = True
-
-fix.db.define_item("NUMLOWALARM", "Number OK", 'float', 0, 100, 'degC', 50000, "Min,Max,highWarn,highAlarm,lowWarn,lowAlarm")
-fix.db.set_value("NUMLOWALARM", 50.19)
-fix.db.get_item("NUMLOWALARM").old = False
-fix.db.get_item("NUMLOWALARM").bad = False
-fix.db.get_item("NUMLOWALARM").fail = False
-fix.db.get_item("NUMLOWALARM").set_aux_value("Min",40)
-fix.db.get_item("NUMLOWALARM").set_aux_value("lowAlarm",60)
-fix.db.get_item("NUMLOWALARM").set_aux_value("lowWarn",70)
-fix.db.get_item("NUMLOWALARM").set_aux_value("highWarn",80)
-fix.db.get_item("NUMLOWALARM").set_aux_value("highAlarm",90)
-fix.db.get_item("NUMLOWALARM").set_aux_value("Max",100)
-
-fix.db.define_item("NUMLOWWARN", "Number OK", 'float', 0, 100, 'degC', 50000, "Min,Max,highWarn,highAlarm,lowWarn,lowAlarm")
-fix.db.set_value("NUMLOWWARN", 50.19)
-fix.db.get_item("NUMLOWWARN").old = False
-fix.db.get_item("NUMLOWWARN").bad = False
-fix.db.get_item("NUMLOWWARN").fail = False
-fix.db.get_item("NUMLOWWARN").set_aux_value("Min",40)
-fix.db.get_item("NUMLOWWARN").set_aux_value("lowAlarm",45)
-fix.db.get_item("NUMLOWWARN").set_aux_value("lowWarn",55)
-fix.db.get_item("NUMLOWWARN").set_aux_value("highWarn",80)
-fix.db.get_item("NUMLOWWARN").set_aux_value("highAlarm",90)
-fix.db.get_item("NUMLOWWARN").set_aux_value("Max",100)
-
-
-fix.db.define_item("NUMMAX", "Number OK", 'float', 0, 100, 'degC', 50000, "Min,Max,highWarn,highAlarm,lowWarn,lowAlarm")
-fix.db.set_value("NUMMAX", 50.19)
-fix.db.get_item("NUMMAX").old = False
-fix.db.get_item("NUMMAX").bad = False
-fix.db.get_item("NUMMAX").fail = False
-fix.db.get_item("NUMMAX").set_aux_value("Max",40)
+create_numbers("NUMOK", 50.91)
+create_numbers("NUMOLD", 51.82, old=True)
+create_numbers("NUMBAD", 48.73, bad=True)
+create_numbers("NUMFAIL", 65.64, fail=True)
+create_numbers("NUMANNUNCIATE", 62.55, annunciate=True)
+create_numbers("NUMLOWWARN",18.46)
+create_numbers("NUMLOWALARM", 5.37)
+create_numbers("NUMHIGHWARN", 81.28)
+create_numbers("NUMHIGHALARM", 95.19)
 
 
 
