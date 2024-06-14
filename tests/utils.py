6cb46c3336cf4e295f5d6091967acd547a9e9b59
Eric Blevins <eblevins@kmilearning.com>
2024-06-14 17:53:17 -0400

    Added more compare options to the tracker utility

--- tests/utils.py
+++ tests/utils.py
@@ -13,13 +13,23 @@ class CallTracker:
             return method(obj, *args, **kwargs)
 
         setattr(obj, method_name, wrapper)
+    def was_called(self, method_name):
+        return any( call[0] == method_name for call in self.calls)
+
+    def was_not_called(self, method_name):
+        return not any( call[0] == method_name for call in self.calls)
 
     def was_called_with(self, method_name, *args, **kwargs):
         return any(call[0] == method_name and call[1] == args for call in self.calls)
 
+    def was_not_called_with(self, method_name, *args, **kwargs):
+        return not any(call[0] == method_name and call[1] == args for call in self.calls)
 @contextmanager
 def track_calls(obj, method_name):
+    if not isinstance(method_name, list):
+        method_name = [method_name]
     tracker = CallTracker()
-    tracker.track(obj, method_name)
+    for m in method_name:
+        tracker.track(obj, m)
     yield tracker
 
