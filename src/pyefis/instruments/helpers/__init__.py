0a0922faa69e1b8e011bc16d500373e409cde9b3
Eric Blevins <eblevins@kmilearning.com>
2024-06-18 18:28:35 -0400

    Updated font size calcualtion for performance and added option for numbers vs text to get the largest possible size

--- src/pyefis/instruments/helpers/__init__.py
+++ src/pyefis/instruments/helpers/__init__.py
@@ -1,58 +1,47 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-def fit_to_mask(width,height,mask,font,units_mask=None, units_ratio=0.8):
-    # This works well for text, but renderes numbers undersized
-    font_size = height
-    text_font = QFont(font)
-    text_font.setPointSizeF(font_size)
-    units_font = QFont(font)
-    units_font.setPointSizeF(font_size * units_ratio)
-    fm = QFontMetrics(text_font)
-    text_width = fm.boundingRect(mask).width()
-    text_height = fm.boundingRect(mask).height() - fm.leading()
-    units_width = 0
-    units_height = 0
-    if units_mask:
-        fmu = QFontMetrics(units_font)
-        units_width = fmu.boundingRect(units_mask).width()
-        units_height = fmu.boundingRect(units_mask).height() 
-
-    # IF needed, shrink until it fits
-    while ( (text_width + units_width > width) or text_height > height ) and font_size > 0.5:
-        font_size -= 0.2
+def fit_to_mask(width,height,mask,font,units_mask=None, units_ratio=0.8, numeric=False):
+    font_size = 100
+    error = 100
+    minerror = 100
+    goal = 0.02
+    count = 0
+    while ( error > goal):
+        count += 1
+        text_font = QFont(font)
         text_font.setPointSizeF(font_size)
-        fm = QFontMetrics(text_font)
-        text_width = fm.boundingRect(mask).width()
-        text_height = fm.boundingRect(mask).height() 
+        fm = QFontMetricsF(text_font)
+        text_width = fm.horizontalAdvance(mask)
+        if numeric:
+            text_height = fm.tightBoundingRect(mask).height()
+        else:
+            text_height = fm.boundingRect(mask).height()
+        units_width = 0
+        units_height = 0
         if units_mask:
+            units_font = QFont(font)
             units_font.setPointSizeF(font_size * units_ratio)
-            fmu = QFontMetrics(units_font)
-            units_width = fmu.boundingRect(units_mask).width()
-            units_height = fmu.boundingRect(units_mask).height() 
-    # If needed, grow until it fills
-    while (text_width + units_width < width) and text_height < height:
-        font_size += 0.2
-        text_font.setPointSizeF(font_size)
-        fm = QFontMetrics(text_font)
-        text_width = fm.boundingRect(mask).width()
-        text_height = fm.boundingRect(mask).height() 
-        if units_mask:
-            units_font.setPointSizeF(font_size * units_ratio)
-            fmu = QFontMetrics(units_font)
-            units_width = fmu.boundingRect(units_mask).width()
-            units_height = fmu.boundingRect(units_mask).height() 
+            fmu = QFontMetricsF(units_font)
+            units_width = fmu.horizontalAdvance(units_mask)
+        hfactor = height / text_height
+        wfactor = width / ( text_width + units_width)
+        if hfactor < 1 and wfactor < 1:
+            factor = min(hfactor, wfactor)
+        elif hfactor > 1 and wfactor > 1:
+            factor = max(hfactor,wfactor)
+        elif hfactor < 1 and wfactor > 1:
+            factor = hfactor
+        else:
+            factor = wfactor
+        error = abs(factor - 1)
+        if factor > 1:
+            if error < minerror:
+                minerror = error
+            else:
+                break
+        font_size = font_size * factor
+    return font_size
 
-    font_size -= 0.2
-
-    # The above took care of the width, this addresses the height:
-    while (text_height >= height) and font_size > 0.5:
-        font_size -= 0.2
-        text_font.setPointSizeF(font_size)
-        fm = QFontMetrics(text_font)
-        text_height = fm.boundingRect(mask).height() 
-    if font_size <= 0:
-        raise "Error"
-    return(font_size)
 
 
